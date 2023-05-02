a4c58f1ad6b580d786ae2f9ae972cd79cff24db3
Dzem-z <egneradam@gmail.com>
2023-05-02 14:44:50 +0200

    added nextList_nextListSetAsCurrent test

--- app/src/test/java/com/example/prioritylist/DataManagerTests.kt
+++ app/src/test/java/com/example/prioritylist/DataManagerTests.kt
@@ -518,4 +518,29 @@ class DataManagerTests {
 
     }
 
+    @Test
+    fun nextList_nextListSetAsCurrent() {
+        val manager = DataManager()
+
+        val name1 = "name1"; val type1 = TaskTypes.PRIORITY
+        val name2 = "name2"; val type2 = TaskTypes.DEADLINE
+        val name3 = "name3"; val type3 = TaskTypes.DEADLINE_PRIORITY
+
+        manager.addListUseCase(0, name3, type3)
+        manager.addListUseCase(0, name2, type2)
+        manager.addListUseCase(0, name1, type1)
+
+        manager.nextListUseCase()
+
+        val newName1 = manager.getNameUseCase()
+
+        assertTrue(newName1 == name2)
+
+        manager.nextListUseCase()
+
+        val newName2 = manager.getNameUseCase()
+
+        assertTrue(newName2 == name3)
+    }
+
 }
