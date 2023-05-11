fe147da6a2c3e4c4d7e4ce05221fe63b8eca2471
Dzem-z <egneradam@gmail.com>
2023-05-11 12:20:09 +0200

    added backbone of navigation and floatingPointButton

--- app/src/main/java/com/example/prioritylist/ui/MainPageScreen.kt
+++ app/src/main/java/com/example/prioritylist/ui/MainPageScreen.kt
@@ -1,17 +1,23 @@
 package com.example.prioritylist.ui
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prioritylist.data.MainPage
 import androidx.compose.ui.Modifier
-
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 
 @Composable
@@ -36,9 +42,36 @@ fun MainPageScreen(modifier: Modifier = Modifier) {
             )
 
 
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = "Drawer Icon"
+                )
+            }
         }
+
     ){ innerPadding ->
-        ListContainer(modifier = modifier.padding(innerPadding))
+        val navController = rememberNavController()
+        val backStackEntry by navController.currentBackStackEntryAsState()
+
+        NavHost(
+            navController = navController,
+            startDestination = "ListContainer",
+            modifier = modifier
+        ){
+            composable(route = "ListContainer"){
+                ListContainer(modifier = modifier.padding(innerPadding))
+            }
+
+            composable(route = "AddTask"){
+
+            }
+        }
+
+
 
     }
 }
