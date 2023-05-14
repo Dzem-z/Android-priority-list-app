78f3a155bf992e03835b36938b9f554a658c3fd5
Dzemz-z <egneradam@gmail.com>
2023-05-14 16:24:16 +0200

    implemented add task route

--- app/src/main/java/com/example/prioritylist/ui/MainPageScreen.kt
+++ app/src/main/java/com/example/prioritylist/ui/MainPageScreen.kt
@@ -14,66 +14,50 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prioritylist.data.MainPage
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.prioritylist.data.PriorityTask
+import java.time.LocalDateTime
 
 
 @Composable
-fun MainPageScreen(modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { /*TODO*/ },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { /*TODO*/ }
-                    ) {
-                      Icon(
-                          imageVector = Icons.Rounded.Menu,
-                          contentDescription = "Drawer Icon"
-                          )
-                    }
-                },
-                actions = {
-                    /*TODO*/
+fun MainPageScreen(holder: StateHolder = StateHolder(), modifier: Modifier = Modifier) {
+
+
+
+    val navController = rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    NavHost(
+        navController = navController,
+        startDestination = "ListContainer",
+        modifier = modifier
+    ){
+        composable(route = "ListContainer"){
+            ListContainer(holder = holder, onAddTask = {
+                holder.onAddTask()
+                navController.navigate("AddTask")
+            })
+        }
+
+        composable(route = "AddTask"){
+            EditTaskScreen(
+                holder = holder,
+                onConfirmMessage = "add task",
+                onConfirm = {
+                    holder.addTask()
+                    navController.popBackStack()
                 }
             )
-
-
-        },
-
-        floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.Rounded.Add,
-                    contentDescription = "Drawer Icon"
-                )
-            }
         }
-
-    ){ innerPadding ->
-        val navController = rememberNavController()
-        val backStackEntry by navController.currentBackStackEntryAsState()
-
-        NavHost(
-            navController = navController,
-            startDestination = "ListContainer",
-            modifier = modifier
-        ){
-            composable(route = "ListContainer"){
-                ListContainer(modifier = modifier.padding(innerPadding))
-            }
-
-            composable(route = "AddTask"){
-
-            }
-        }
-
-
-
     }
+
+
+
+
 }
 
 @Composable
