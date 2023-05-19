7c3292009f083bd698c446bb0277a50eda190cc7
Dzem-z <egneradam@gmail.com>
2023-05-19 15:17:34 +0200

    migration from LocalDateTime to Date

--- app/src/test/java/com/example/prioritylist/StateHolderTests.kt
+++ app/src/test/java/com/example/prioritylist/StateHolderTests.kt
@@ -5,6 +5,7 @@ import com.example.prioritylist.data.TaskTypes
 import com.example.prioritylist.ui.StateHolder
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 
 class StateHolderTests {
@@ -12,21 +13,21 @@ class StateHolderTests {
     @Test
     fun getList_listReadProperly() {
         val task1 = PriorityTask(
-            dateOfCreation =  LocalDateTime.parse("2023-03-12T12:15:30"),
+            dateOfCreation =  SimpleDateFormat("yyyy-MM-ddHH:mm:ss").parse("2023-03-1212:15:30"),
             name = "test_name_1",
             description = "desc_1",
             id = 3,
             priority = 1
         )
         val task2 = PriorityTask(
-            dateOfCreation =  LocalDateTime.parse("2023-03-24T12:15:30"),
+            dateOfCreation =  SimpleDateFormat("yyyy-MM-ddHH:mm:ss").parse("2023-03-2412:15:30"),
             name = "test_name_2",
             description = "desc_2",
             id = 3,
             priority = 1
         )
         val task3 = PriorityTask(
-            dateOfCreation =  LocalDateTime.parse("2023-03-12T12:15:30"),
+            dateOfCreation =  SimpleDateFormat("yyyy-MM-ddHH:mm:ss").parse("2023-03-1212:15:30"),
             name = "test_name_3",
             description = "desc_1",
             id = 3,
@@ -36,12 +37,10 @@ class StateHolderTests {
         val holder = StateHolder()
 
 
-        holder.displayingList = mutableListOf<PriorityTask>(
-            task1, task2, task3
-        )
+        holder.setList(mutableListOf<PriorityTask>(task1, task2, task3))
 
-        assertTrue(holder.currentType.value == TaskTypes.PRIORITY)
-        assertTrue(holder.displayingList[0] == task1 && holder.displayingList[1] == task2 && holder.displayingList[2] == task3)
+        assertTrue(holder.getCurrentType() == TaskTypes.PRIORITY)
+        assertTrue(holder.getList()[0] == task1 && holder.getList()[1] == task2 && holder.getList()[2] == task3)
 
     }
 
