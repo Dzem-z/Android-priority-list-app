78cc1c415a7a82370f3c9b2a8a6895da57137be5
Dzem-z <egneradam@gmail.com>
2023-05-19 15:22:30 +0200

    added abstract accessors to enum types. Now enum can check if specified field is present in associated task class

--- app/src/main/java/com/example/prioritylist/data/TaskTypes.kt
+++ app/src/main/java/com/example/prioritylist/data/TaskTypes.kt
@@ -7,10 +7,38 @@ TODO(comments)
  */
 
 enum class TaskTypes(val taskType: KClass<out Task>, val listType: KClass<out TaskList<*>>) {
-    CATEGORY(CategoryTask::class, CategoryTaskList::class),
-    PRIORITY(PriorityTask::class, PriorityTaskList::class),
-    DEADLINE_PRIORITY_CATEGORY(DeadlinePriorityCategoryTask::class, DeadlinePriorityCategoryTaskList::class),
-    DEADLINE_CATEGORY(DeadlineCategoryTask::class, DeadlineCategoryTaskList::class),
-    DEADLINE_PRIORITY(DeadlinePriorityTask::class, DeadlinePriorityTaskList::class),
-    DEADLINE(DeadlineTask::class, DeadlineTaskList::class)
+    CATEGORY(CategoryTask::class, CategoryTaskList::class){
+        override fun hasPriority() = false
+        override fun hasCategory() = true
+        override fun hasDeadline() = false
+                                                          },
+    PRIORITY(PriorityTask::class, PriorityTaskList::class){
+        override fun hasPriority() = true
+        override fun hasCategory() = false
+        override fun hasDeadline() = false
+                                                          },
+    DEADLINE_PRIORITY_CATEGORY(DeadlinePriorityCategoryTask::class, DeadlinePriorityCategoryTaskList::class){
+        override fun hasPriority() = true
+        override fun hasCategory() = true
+        override fun hasDeadline() = true
+                                                                                                            },
+    DEADLINE_CATEGORY(DeadlineCategoryTask::class, DeadlineCategoryTaskList::class){
+        override fun hasPriority() = false
+        override fun hasCategory() = true
+        override fun hasDeadline() = true
+                                                                                    },
+    DEADLINE_PRIORITY(DeadlinePriorityTask::class, DeadlinePriorityTaskList::class){
+        override fun hasPriority() = true
+        override fun hasCategory() = false
+        override fun hasDeadline() = true
+                                                                                   },
+    DEADLINE(DeadlineTask::class, DeadlineTaskList::class){
+        override fun hasPriority() = false
+        override fun hasCategory() = false
+        override fun hasDeadline() = true
+                                                            };
+
+    abstract fun hasPriority(): Boolean
+    abstract fun hasCategory(): Boolean
+    abstract fun hasDeadline(): Boolean
 }
