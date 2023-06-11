871e20c674f20b848235360c50518ca0e0c36c24
Dzem-z <egneradam@gmail.com>
2023-06-11 22:01:41 +0200

    file containing interfaces of repositories

---
+++ app/src/main/java/com/example/prioritylist/data/database/Repositories.kt
@@ -0,0 +1,28 @@
+package com.example.prioritylist.data.database
+
+import com.example.prioritylist.data.database.ListEntity
+import com.example.prioritylist.data.database.TaskEntity
+import kotlinx.coroutines.flow.Flow
+
+interface MainRepository {
+
+    fun loadListByID(listID: Int): Flow<List<TaskEntity>>
+
+    fun loadListCredentials(): Flow<List<ListEntity>>
+
+    suspend fun saveCurrent(listID: Int, newCurrent: Int)
+
+    suspend fun saveList(list: ListEntity)
+}
+
+interface ListRepository {
+    suspend fun add(task: TaskEntity)
+
+    suspend fun delete(task: TaskEntity)
+
+    suspend fun changeName(listID: Int, newName: String)
+}
+
+interface HistoryRepository {
+    //TODO
+}
