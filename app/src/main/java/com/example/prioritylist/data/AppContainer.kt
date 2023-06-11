67d988d80c7f3639fd98e3631571e75009e3a64a
Dzem-z <egneradam@gmail.com>
2023-06-11 21:52:46 +0200

    an AppContainer that provides MainRepository and ListRepository to the app

---
+++ app/src/main/java/com/example/prioritylist/data/AppContainer.kt
@@ -0,0 +1,39 @@
+package com.example.prioritylist.data
+
+import android.content.Context
+import com.example.prioritylist.data.database.ListRepository
+import com.example.prioritylist.data.database.MainRepository
+import com.example.prioritylist.data.database.OfflineListRepository
+import com.example.prioritylist.data.database.OfflineMainRepository
+import com.example.prioritylist.data.database.database
+
+/**
+ * App container for Dependency injection.
+ */
+
+interface AppContainer {
+    val mainRepository: MainRepository
+    val listRepository: ListRepository
+}
+
+/**
+ * [AppContainer] implementation that provides instance of [OfflineMainRepository]
+ * and [OfflineListRepository]
+ */
+
+class AppDataContainer(private val context: Context): AppContainer {
+    /**
+     * Implementation for [MainRepository]
+     */
+
+    override val mainRepository: MainRepository by lazy {
+        OfflineMainRepository(database.getDatabase(context).mainDao())
+    }
+    /**
+     * Implementation for [ListRepository]
+     */
+    override val listRepository: ListRepository by lazy {
+        OfflineListRepository(database.getDatabase(context).listDao())
+    }
+
+}
